bc9670483fb4f766dacd3fddc3ce771e3c4c1b29
Kevin Huang <41244296+kh9sd@users.noreply.github.com>
2024-03-08 15:48:16 -0500

    Q1 a

--- Q1.py
+++ Q1.py
@@ -23,8 +23,25 @@ def q1_a(P: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     '''
 
     ### Enter code below
-    center = np.array([0,1,0])
-    normal = np.array([0,0,1])
+   
+    center = np.mean(P, axis=0)
+    assert center.shape == (3,)
+
+    covariance_matrix = np.zeros((3,3))
+
+    for pt in P:
+        assert pt.shape == (3,)
+
+        pt_to_center_diff = np.expand_dims(pt - center, axis=0)
+        assert pt_to_center_diff.shape == (1,3)
+
+        # transpose first one, bc first should be a column vector
+        covariance_matrix += pt_to_center_diff.T @ pt_to_center_diff
+    
+    eigenvals, eigenvecs = np.linalg.eig(covariance_matrix)
+    # eigenvecs are the COLUMNS of the returned matrix, have to transpose to iterate properly
+    _, normal = min(zip(eigenvals, eigenvecs.T), key=lambda pair: pair[0])
+
     return normal, center
 
 
